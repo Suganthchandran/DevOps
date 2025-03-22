853488b3ddb83de1fc16cdd1b998fd66bc9cb864
Suganth <suganthchandran07@gmail.com>
2025-03-22 09:50:42 +0530

    Updated

--- TASK-2/deploy.sh
+++ TASK-2/deploy.sh
@@ -2,7 +2,7 @@
     # echo hi123
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u suganth07 -p dckr_pat_oAYT02qRwSz2jrlHz54liPeI7_k
+    # docker login -u suganth07 -p dckr_pat_oAYT02qRwSz2jrlHz54liPeI7_k
     docker tag test1 suganth07/devops
     docker push suganth07/devops
     docker compose up -d
